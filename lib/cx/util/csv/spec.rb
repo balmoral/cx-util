994246f1f83d039147ef0b99d63b5e5af98cca11
balmoral <colgunn@me.com>
2017-01-19 10:56:52 +1100

    initial commit

--- lib/cx/util/csv/spec.rb
+++ lib/cx/util/csv/spec.rb
@@ -1,6 +1,6 @@
 require 'cx/util/csv/field'
 require 'cx/util/csv/reader'
-require 'cx/util/stub'
+require 'cx/util/model'
 
 # Example:
 #
@@ -111,38 +111,38 @@ module CX
       end
 
       # RUBY MAGIC WARNING
-      # Returns the stub class for the spec.
+      # Returns the model class for the spec.
       # Default behaviour is to create an anonymous
       # class generated from this spec's fields.
       # This method can be overriden if a subclass
-      # wants to specify a bespoke stub class.
-      def self.stub_class(parent: nil)
-        unless @stub_class
-          @stub_class = Class.new(parent || CX::Stub)
-          @stub_class.set_fields(field_names)
+      # wants to specify a bespoke model class.
+      def self.model_class(parent: nil)
+        unless @model_class
+          @model_class = Class.new(parent || CX::Model)
+          @model_class.attrs = field_names
         end
-        @stub_class
+        @model_class
       end
 
       # Should return a hash which maps between
-      # this spec's field symbols and its stub's
+      # this spec's field symbols and its model's
       # field/method names. The hash only needs to
       # map those fields which have different names.
       # Default here is to return nil (no map).
       # Subclasses may override.
-      def self.stub_map
+      def self.model_map
         nil
       end
 
-      # Returns an array of stub 'attributes' (field/method names)
+      # Returns an array of model 'attributes' (field/method names)
       # corresponding to each field in this csv spec.
       # If a field has a null type then the selector will be nil.
       # If a map is given, then it should map from each field sym
-      # in this spec to a field (attribute) name in the stub.
+      # in this spec to a field (attribute) name in the model.
       # If the map is given but does not contain a mapping for a field,
-      # then the field's sym will be used as the stub attribute.
-      def self.stub_attributes(map: nil)
-        _map = map || self.stub_map
+      # then the field's sym will be used as the model attribute.
+      def self.model_attributes(map: nil)
+        _map = map || self.model_map
         fields.collect do |f|
           if f.null?
             nil
@@ -153,41 +153,41 @@ module CX
         end
       end
 
-      # Returns an array of stubs from the given rows.
-      # If a stub class is not given it will be this spec's
-      # default stub class, an anonymous class generated
+      # Returns an array of models from the given rows.
+      # If a model class is not given it will be this spec's
+      # default model class, an anonymous class generated
       # orthogonally from this spec's fields. The optional
       # map (if given) should map from the field sym's
-      # in this spec to the field name in the stub.
-      def self.stubs_from_rows(rows, stub: nil, map: nil)
+      # in this spec to the field name in the model.
+      def self.models_from_rows(rows, model_class: nil, map: nil)
         # puts "#{name}###{__method__}(rows.size = #{rows.size})"
         result = []
-        stub_class = stub || self.stub_class
-        attrs = stub_attributes(map: map)
+        model_class ||= self.model_class
+        attrs = model_attributes(map: map)
         rows.each do |row|
           args = {}
           row.each_with_index do |v,i|
             attr = attrs[i]
             args[attr] = v if attr
           end
-          result << stub_class.new(**args)
+          result << model_class.new(**args)
         end
         result
       end
 
-      def self.stubs_from_file(path, stub: nil, map: nil)
+      def self.models_from_file(path, model_class: nil, map: nil)
         rows = rows_from_file(path)
-        stubs_from_rows(rows, stub: stub, map: map)
+        models_from_rows(rows, model_class: model_class, map: map)
       end
 
-      def self.stubs_from_string(str, stub: nil, map: nil)
+      def self.models_from_string(str, model_class: nil, map: nil)
         rows = rows_from_string(str)
-        stubs_from_rows(rows, stub: stub, map: map)
+        models_from_rows(rows, model_class: model_class, map: map)
       end
 
-      def self.stubs_from_stream(io, stub: nil, map: nil)
+      def self.models_from_stream(io, model_class: nil, map: nil)
         rows = rows_from_stream(io)
-        stubs_from_rows(rows, stub: stub, map: map)
+        models_from_rows(rows, model_class: model_class, map: map)
       end
 
     end
